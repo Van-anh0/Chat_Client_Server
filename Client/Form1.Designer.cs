30e953dd1abe36e2776862c2bf5d82491897c654
VanAnh <1910115@dlu.edu.vn>
2022-04-26 10:18:37 +0700

    Add project files.

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace Client
+{
+	partial class Client_form
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txbMessage = new System.Windows.Forms.TextBox();
+            this.lsvMessage = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(468, 340);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(100, 28);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txbMessage
+            // 
+            this.txbMessage.Location = new System.Drawing.Point(16, 342);
+            this.txbMessage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txbMessage.Name = "txbMessage";
+            this.txbMessage.Size = new System.Drawing.Size(443, 22);
+            this.txbMessage.TabIndex = 4;
+            // 
+            // lsvMessage
+            // 
+            this.lsvMessage.HideSelection = false;
+            this.lsvMessage.Location = new System.Drawing.Point(3, 1);
+            this.lsvMessage.Name = "lsvMessage";
+            this.lsvMessage.Size = new System.Drawing.Size(580, 334);
+            this.lsvMessage.TabIndex = 6;
+            this.lsvMessage.UseCompatibleStateImageBehavior = false;
+            this.lsvMessage.View = System.Windows.Forms.View.List;
+            // 
+            // Client_form
+            // 
+            this.AcceptButton = this.btnSend;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 386);
+            this.Controls.Add(this.lsvMessage);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txbMessage);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Client_form";
+            this.Text = "Client";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Client_form_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnSend;
+		private System.Windows.Forms.TextBox txbMessage;
+        private System.Windows.Forms.ListView lsvMessage;
+    }
+}
+
